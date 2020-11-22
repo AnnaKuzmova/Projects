25346b64e1ac21903508af4e39e8e87f584e3c41
AnnaKuzmova <anni.kuzmovastd@gmail.com>
2020-11-22 18:19:09 +0200

    JapaMapa Project Added

---
+++ JapaMapa/index.js
@@ -0,0 +1,261 @@
+let jpnPrefectures = [
+    {
+        title: 'Hiroshima',
+        src: 'https://images.unsplash.com/photo-1568295936182-e154641caa60?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
+        description : 'Very nice and beautiful'
+    },
+    {
+        title: 'Okayama',
+        src: 'https://images.unsplash.com/photo-1562887059-f11cd4f5e989?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=967&q=80',
+        description : 'Sunny and Funny'
+    },
+    {
+        title: 'Shimane',
+        src: 'https://images.unsplash.com/photo-1603434749789-b0ebbec89484?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1051&q=80',
+        description : 'Lovely Gardens'
+    },
+    {
+        title: 'Tottori',
+        src: 'https://images.unsplash.com/photo-1605931564173-8260fc3fb4bf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
+        description : 'Amazing little gardens'
+    },
+    {
+        title: 'Yamaguchi',
+        src: 'https://images.unsplash.com/photo-1529578664164-410c379f3f44?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
+        description : 'Beautiful compositions'
+    },
+    {
+        title: 'Saga',
+        src: 'https://images.unsplash.com/photo-1489806899098-6956bd2a6691?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
+        description : 'Lovely train routes'
+    },
+    {
+        title: 'Fukuoka',
+        src: 'https://images.unsplash.com/photo-1593501370110-8f7ca43309a5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
+        description : 'Amazinng Temples'
+    },
+    {
+        title: 'Kumamoto',
+        src: 'https://images.unsplash.com/photo-1600506639250-73e7ecf74dd8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
+        description : 'Once a lifetime expiriences'
+    }
+    ,{
+        title: 'Miyazaki',
+        src: 'https://images.unsplash.com/photo-1598331802421-9b06d0171c77?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
+        description : 'Nature Temples'
+    }
+    ,{
+        title: 'Ehime',
+        src: 'https://images.unsplash.com/photo-1589702925893-4f1e70c7b42f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
+        description : 'Peacful and small villages'
+    }
+    ,{
+        title: 'Kagawa',
+        src: 'https://images.unsplash.com/photo-1593958866187-37e5c3b98278?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1189&q=80',
+        description : 'Clear views'
+    },{
+        title: 'Kochi',
+        src: 'https://images.unsplash.com/photo-1480796927426-f609979314bd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
+        description : 'Small and lovely shops'
+    },
+    {
+        title: 'Oita',
+        src: 'https://images.unsplash.com/photo-1564810796095-ac4435ec654e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1052&q=80',
+        description : 'Small but lovely attractions'
+    },
+    {
+        title: 'Tokushima',
+        src: 'https://images.unsplash.com/photo-1572142569473-8ec0be59d443?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1047&q=80',
+        description : 'Frozen over time old places'
+    },
+    {
+        title: 'Aichi',
+        src: 'https://images.unsplash.com/photo-1600418288086-111f9256c817?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1055&q=80',
+        description : 'Modern and big buildings'
+    },
+    {
+        title: 'Gifu',
+        src: 'https://images.unsplash.com/photo-1593579329655-d4ebec1d4c94?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1189&q=80',
+        description : 'Breathtaking landscapes in the countryside'
+    },
+    {
+        title: 'Ishikawa',
+        src: 'https://images.unsplash.com/photo-1596048236876-2afb418c0dce?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=967&q=80',
+        description : 'Asthonoshing gardens'
+    },
+    {
+        title: 'Mie',
+        src: 'https://images.unsplash.com/photo-1493780474015-ba834fd0ce2f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1026&q=80',
+        description : 'Lovely places to visit'
+    },
+    {
+        title: 'Nagano',
+        src: 'https://images.unsplash.com/photo-1563282653-57d1e6f6896d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
+        description : 'Amazing historical castles'
+    },
+    {
+        title: 'Shizuoka',
+        src: 'https://images.unsplash.com/photo-1584341985917-5e50c008deb2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
+        description : 'Amazing mountains'
+    },
+    {
+        title: 'Toyama',
+        src: 'https://images.unsplash.com/photo-1543202416-ba969b3eb8cb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1053&q=80',
+        description : 'One of a kind festivals'
+    },
+    {
+        title: 'Hokkaido',
+        src: 'https://images.unsplash.com/photo-1576829021154-ed73d379e1c0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
+        description : 'Snowy and lovely'
+    },
+    {
+        title: 'Fukui',
+        src: 'https://images.unsplash.com/photo-1589806037795-d082cf2379d2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=992&q=80',
+        description : 'Amazing forests to explore'
+    },
+    {
+        title: 'Hyōgo',
+        src: 'https://images.unsplash.com/photo-1524413840807-0c3cb6fa808d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
+        description : 'Colorful and cheerful'
+    },
+    {
+        title: 'Kyoto',
+        src: 'https://images.unsplash.com/photo-1558862107-d49ef2a04d72?ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
+        description : 'Countless temples and monuments'
+    },
+    {
+        title: 'Nara',
+        src: 'https://images.unsplash.com/photo-1528534622447-0db07cd82ad5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
+        description : 'All kinds of animals'
+    },
+    {
+        title: 'Osaka',
+        src: 'https://images.unsplash.com/photo-1537153960513-344d9f2e8216?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
+        description : 'Amazing lights and night life'
+    },
+    {
+        title: 'Shiga',
+        src: 'https://images.unsplash.com/photo-1605762750628-118a59905d67?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
+        description : 'Perfect place for autumn'
+    },
+    {
+        title: 'Wakayama',
+        src: 'https://images.unsplash.com/photo-1580023095882-fe2d37a88aa8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=967&q=80',
+        description : 'Amazing ancient graves'
+    },
+    {
+        title: 'Chiba',
+        src: 'https://images.unsplash.com/photo-1580873317758-4205f2e11f24?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1189&q=80',
+        description : 'Beautiful sea views'
+    },
+    {
+        title: 'Ibaraki',
+        src: 'https://images.unsplash.com/photo-1581927569419-2ca533052e93?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
+        description : 'Lovely flowers to see'
+    },
+    {
+        title: 'Kanagawa',
+        src: 'https://images.unsplash.com/photo-1583901378448-bf08796ea1df?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=967&q=80',
+        description : 'Amazing mountines and hills'
+    },
+    {
+        title: 'Saitama',
+        src: 'https://images.unsplash.com/photo-1599029229231-768772de07f7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
+        description : 'Cute hand crafts'
+    },
+    {
+        title: 'Tochigi',
+        src: 'https://images.unsplash.com/photo-1573011352979-b24361f0cef4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
+        description : 'Beautiful autumn views'
+    },
+    {
+        title: 'Tokyo',
+        src: 'https://images.unsplash.com/photo-1540959733332-eab4deabeeaf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1071&q=80',
+        description : 'Everything you must expirience'
+    },
+    {
+        title: 'Yamanashi',
+        src: 'https://images.unsplash.com/photo-1567056279735-7a90ad20d10b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1055&q=80',
+        description : 'Amazing and beautiful eco walks'
+    },
+    {
+        title: 'Akita',
+        src: 'https://images.unsplash.com/photo-1598538533536-0797be12f4d4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1051&q=80',
+        description : 'Just amazing'
+    },
+    {
+        title: 'Aomori',
+        src: 'https://images.unsplash.com/photo-1598761494170-7d5fe80511e6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
+        description : 'Unique greenery areas to explore'
+    },
+    {
+        title: 'Fukushima',
+        src: 'https://images.unsplash.com/photo-1584441694882-f12f6c736413?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
+        description : 'Amazing rice fields and historical places'
+    },
+    {
+        title: 'Iwate',
+        src: 'https://images.unsplash.com/photo-1583965174400-7a2b15237658?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1188&q=80',
+        description : 'Cute little lakes to explore'
+    },
+    {
+        title: 'Miyagi',
+        src: 'https://images.unsplash.com/photo-1510208226765-721d3d7d8d7e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
+        description : 'Lovely fox villages'
+    },
+    {
+        title: 'Niigata',
+        src: 'https://images.unsplash.com/photo-1562678556-b604f5c27ddb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
+        description : 'Destination to connect to the nature self'
+    },
+    {
+        title: 'Yamagata',
+        src: 'https://images.unsplash.com/photo-1602001956423-a81bbeb72167?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
+        description : 'Ancient bridges to cross'
+    },
+    {
+        title: 'Nagasaki',
+        src: 'https://images.unsplash.com/photo-1599638517474-6ceccf494065?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1053&q=80',
+        description : 'Historical destionations to visit'
+    },
+    {
+        title: 'Kagoshima',
+        src: 'https://images.unsplash.com/photo-1578286676359-08ea352602f0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
+        description : 'Amazing flowers to discover'
+    },
+    {
+        title: 'Okinawa',
+        src: 'https://images.unsplash.com/photo-1568641922612-4fd5ab7f5b39?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=967&q=80',
+        description : 'Amazing and warm beaches'
+    },
+    {
+        title: 'Gunma',
+        src: 'https://images.unsplash.com/photo-1580052380706-16fcc67ee1da?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
+        description : 'Amazinf and delicious foods'
+    },
+];
+
+
+let path = document.querySelectorAll('svg path');
+path.forEach(path => console.log(path.getAttribute('name')));
+
+let svg = document.querySelector('svg');
+let information = document.querySelector('#information')
+let title = document.querySelector('.title');
+let description = document.querySelector('.description');
+
+svg.addEventListener('mouseover', function(e){
+    let path = e.target.getAttribute('name');
+    if(path == null) {
+        return false;
+    }
+
+    let details = jpnPrefectures.find(item => item.title == path);
+    if(details == undefined) {
+        return false;
+    }
+    information.style.backgroundImage = `url(${details.src})`;
+    title.innerText = details.title;
+    description.innerText = details.description;
+
+});
